3d35e26e1010f4166853a7c1cd501f2747d75a68
alextor26 <alextor2625@gmail.com>
2024-05-25 17:56:36 -0400

    Working on header

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const NavBar = () => {
+  return (
+    <div className="landing-page-nav-container">
+        <header className="dia-dev-design-title">Día Dev & Design</header>
+        <button className="contact-us-button">Contact Us</button>
+    </div>
+  )
+}
+
+export default NavBar
