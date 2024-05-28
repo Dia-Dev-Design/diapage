4cb3ce10d07195d81a4ba437ff757c6d50fb2990
alextor26 <alextor2625@gmail.com>
2024-05-27 21:42:10 -0400

    Created and Styled AboutUs

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import "./App.css";
 import NavBar from "./components/NavBar";
 import Header from "./components/Header";
+import AboutUs from "./components/AboutUs";
 
 function App() {
   return (
     <div className="landing-page-container">
-      <NavBar />
-      <Header />
+      <NavBar className="navbar"/>
+      <Header className="header"/>
+      <AboutUs className="about-us"/>
     </div>
   );
 }
---
+++ src/components/AboutUs.jsx
@@ -0,0 +1,15 @@
+const AboutUs = () => {
+  return (
+      <section className="landing-page-about-us-container">
+        <span className="section-title-thin">We Are</span>
+        <p className="landing-page-about-us-description">
+          At eos omnis autem non ut. Ullam sed non. Aut in in voluptatem
+          asperiores praesentium iure quos dolores cumque. Quibusdam voluptates
+          omnis dicta omnis ad quis omnis minus et. Voluptatem corrupti ut totam
+          deleniti ut praesentium.
+        </p>
+      </section>
+  );
+};
+
+export default AboutUs;
