4c667471f90ca4f9b8a97919146493e0b30f6bc6
Luis Emmanuel <johndoe@example.comgit config --global user.email lerodriguezreyes@outlook.com>
2024-05-29 00:35:16 -0400

    About component draft. Need to check responsiveness across different screens.

---
+++ src/components/About.jsx
@@ -0,0 +1,22 @@
+import "../styles/AboutSection.css"
+
+function About() {
+  return (
+    <section class="about">
+      <div class="aboutContainer">
+        <div class="aboutHeaderContainer">
+          <p class="aboutHeader">We are</p>
+        </div>
+        <div class="aboutParagraphsContainer">
+          <p class="aboutParagraph">
+          We are committed to empowering your business with cutting-edge solutions that align with your goals. Our primary focus is on delivering customized technology that caters to your unique requirements. </p>
+          <p class="aboutParagraph">
+          Simultaneously we support our community through continuous learning and creative collaboration, turning our efforts into sustainable, fulfilling careers and successful businesses.
+          </p>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default About;
