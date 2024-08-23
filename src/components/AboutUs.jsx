06ea41900b9e2b10df785631184711e04d45d0c6
AlfG71 <56656187+AlfG71@users.noreply.github.com>
2024-08-23 14:10:14 -0400

    styled components about us section

--- src/components/AboutUs.jsx
+++ src/components/AboutUs.jsx
@@ -1,8 +1,51 @@
+import styled from "styled-components";
+
+const AboutUsContainer = styled.section`
+  display: flex;
+  flex-wrap: wrap;
+  top: 10%;
+  width: 100%;
+  gap: 3%;
+  position: relative;
+  left: 10%;
+
+  margin-top: 2rem;
+
+  @media (max-width: 1200px) {
+    .landing-page-about-us-container {
+      top: 10vh;
+    }
+  }
+
+  @media (max-width: 1100px) {
+    .landing-page-about-us-container {
+      top: 15vh;
+    }
+  }
+
+  @media (max-width: 885px) {
+    .landing-page-about-us-container {
+      top: 20vh;
+    }
+  }
+`
+
+const SectionTitleThin = styled.span`
+  font-weight: 100;
+  font-size: 128px;
+`
+const AboutUsDescription = styled.div`
+  width: 50rem;
+  font-weight: 400;
+  text-align: justify;
+  margin-top: 25px;
+`
+
 const AboutUs = () => {
   return (
-    <section className="landing-page-about-us-container margin-divider-top">
-      <span className="section-title-thin">We Are</span>
-      <div className="landing-page-about-us-description">
+    <AboutUsContainer>
+      <SectionTitleThin>We Are</SectionTitleThin>
+      <AboutUsDescription>
         <p>
           We are committed to empowering your business with cutting-edge
           solutions that align with your goals.
@@ -13,13 +56,13 @@ const AboutUs = () => {
           to your unique requirements.
         </p>
 
-        <p className="margin-divider-top">
+        <p>
           Simultaneously we support our community through continuous learning
           and creative collaboration, turning our efforts into sustainable,
           fulfilling careers and successful businesses.
         </p>
-      </div>
-    </section>
+      </AboutUsDescription>
+    </AboutUsContainer>
   );
 };
 
